7bcdfcefbd268ba23980fde52d1bf30593bb618a
赵迎超 <zhao_yc1@suixingpay.com>
2020-08-06 16:33:44 +0800

    实现简单的双向绑定-2

--- twowaybind/simple/imSimpleBinding.js
+++ twowaybind/simple/imSimpleBinding.js
@@ -17,3 +17,28 @@ Object.keys(zyc).map((key, index) => {
 zyc.name = 'zyc';
 zyc.age = 28;
 
+
+// const input = document.getElementById('input');
+// const p = document.getElementById('p');
+const obj = {};
+
+const newObj = new Proxy(obj, {
+    get: function (target, key, receiver) {
+        console.log(`getting ${key}!`);
+        return Reflect.get(target, key, receiver);
+    },
+    set: function (target, key, value, receiver) {
+        console.log(target, key, value, receiver);
+        if (key === 'text') {
+            // input.value = value;
+            // p.innerHTML = value;
+        }
+        return Reflect.set(target, key, value, receiver);
+    },
+});
+
+newObj.text = 'zyc';
+
+// input.addEventListener('keyup', function (e) {
+//     newObj.text = e.target.value;
+// });
