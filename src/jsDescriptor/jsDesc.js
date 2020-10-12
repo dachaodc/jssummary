f70bfb39dda9ecf9048df8fa4acd598aeab47407
赵迎超 <zhao_yc1@suixingpay.com>
2020-10-12 14:01:01 +0800

    属性描述对象

---
+++ src/jsDescriptor/jsDesc.js
@@ -0,0 +1,101 @@
+var obj = {p: 'a'};
+
+var desc = Object.getOwnPropertyDescriptor(obj, 'p');
+console.log(desc);
+// Object { value: "a",
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// }
+
+var obj2 = Object.defineProperties({}, {
+    p1: {value: 1, enumerable: true},
+    p2: {value: 2, enumerable: false}
+});
+
+var propertyNames = Object.getOwnPropertyNames(obj2);
+// ["p1", "p2"]
+console.log(propertyNames);
+
+var desc2 = Object.getOwnPropertyDescriptor(obj2, 'p2');
+console.log(desc2);
+
+console.log('-----------------------------------------------------------');
+
+console.log(Object.keys([])); // []
+console.log(Object.getOwnPropertyNames([])); // [ 'length' ]
+
+console.log(Object.keys(Object.prototype)); // []
+console.log(Object.getOwnPropertyNames(Object.prototype));
+
+console.log('--------------------------------------------------------------');
+
+var obj3 = Object.defineProperty({}, 'p', {
+    value: 123,
+    writable: false,
+    enumerable: true,
+    configurable: false
+});
+
+console.log(obj3.p); // 123
+
+obj3.p = 246;
+console.log(obj3.p); // 123
+
+
+console.log('--------------------------------------------------------------');
+
+
+var obj4 = Object.defineProperties({}, {
+    p1: {value: 123, enumerable: true},
+    p2: {value: 'abc', enumerable: true},
+    p3: {
+        get: function () {
+            return this.p1 + this.p2
+        },
+        enumerable: true,
+        configurable: true
+    }
+});
+
+console.log(obj4.p1); // 123
+
+console.log(obj4.p2); // "abc"
+
+console.log(obj4.p3); // "123abc"
+
+// 判断是否可枚举
+console.log(obj4.propertyIsEnumerable('p')); // true
+
+console.log(obj4.propertyIsEnumerable('toString')); // false
+
+
+console.log('---------------------obj5--------------------------');
+
+var obj5 = {hello: 'lloll'};
+console.log(Object.getOwnPropertyDescriptor(obj5, 'hello').value);
+
+
+console.log('---------------------obj6--------------------------');
+
+var proto = Object.defineProperty({}, 'foo', {
+    value: 'a',
+    writable: false
+});
+
+var obj6 = Object.create(proto);
+
+obj6.foo = 'b';
+console.log(obj6.foo); // 'a'
+
+
+
+
+
+
+
+
+
+
+
+
