4bab626321333dcae0fc68fa42a4fa7b9231702a
赵迎超 <zhao_yc1@suixingpay.com>
2020-11-09 17:04:55 +0800

    手动实现 new

--- src/new/new.js
+++ src/new/new.js
@@ -113,3 +113,57 @@ console.log('------------------------');
 // console.log(Person.prototype.constructor);
 // console.log(Person);
 
+
+// new 的内部实现：
+// 创建一个空对象，作为将要返回的对象实例。
+// 将这个空对象的原型，指向构造函数的prototype属性。
+// 将这个空对象赋值给函数内部的this关键字。
+// 开始执行构造函数内部的代码。
+// function _new(/* 构造函数 */ constructor, /* 构造函数参数 */ params) {
+//     // 将 arguments 对象转为数组
+//     var args = [].slice.call(arguments);
+//     // 取出构造函数
+//     var constructor = args.shift();
+//     // 创建一个空对象，继承构造函数的 prototype 属性
+//     var context = Object.create(constructor.prototype);
+//     // 执行构造函数
+//     var result = constructor.apply(context, args);
+//     // 如果返回结果是对象，就直接返回，否则返回 context 对象
+//     return (typeof result === 'object' && result != null) ? result : context;
+// }
+//
+// // 实例
+// var actor = _new(Person, '张三', 28);
+// console.log(actor);
+
+
+function _newObj(constructor, pram) {
+    let arrPrams = [].slice.call(arguments);
+    let cons = arrPrams.shift();
+    let context = Object.create(cons.prototype);
+    let obj = cons.apply(context, arrPrams);
+    return (typeof obj === 'object' && obj != null) ? obj : context
+}
+
+const newObj = _newObj(Person, 'zyc', '28');
+
+console.log(newObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
