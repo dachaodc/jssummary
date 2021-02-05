b15cd1fb45aecbc91b357625d49ec2829d295034
赵迎超 <zhao_yc1@suixingpay.com>
2021-02-05 15:38:22 +0800

    1.css 优先级 2.有趣的typeOf

---
+++ src/js/有趣的typeOf/typeOf.js
@@ -0,0 +1,29 @@
+// 想一想 下面的输出结果，分别是多少？
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof {});
+
+console.log(typeof []);
+
+console.log(typeof "");
+
+console.log(typeof 0);
+
+console.log(typeof typeof null);
+
+console.log(typeof typeof undefined);
+
+console.log(typeof typeof typeof null);
+
+// object
+// undefined
+// object
+// object
+// string
+// number
+// string
+// string
+// string
