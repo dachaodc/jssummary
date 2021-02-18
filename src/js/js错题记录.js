fee16089c264add7d27a2bbe353a1244c3d62bee
赵迎超 <zhao_yc1@suixingpay.com>
2021-02-18 09:48:40 +0800

    null == undefined

---
+++ src/js/js错题记录.js
@@ -0,0 +1,14 @@
+// 1.var dd; console.log(dd == undefined) true
+// null == undefined true
+
+var dcdc;
+console.log(dcdc); // undefined
+console.log(zyc);  // undefined
+var zyc;
+// console.log(zycdc); // ReferenceError: zycdc is not defined
+let cc = null;
+console.log(cc);
+
+// 2.
+
+
