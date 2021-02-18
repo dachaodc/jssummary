b21a3adba8f522b168e79b6aaa1c3a945a5f98fb
赵迎超 <zhao_yc1@suixingpay.com>
2021-02-18 10:04:25 +0800

    极限格式转换的判断

--- src/js/js错题记录.js
+++ src/js/js错题记录.js
@@ -9,6 +9,21 @@ var zyc;
 let cc = null;
 console.log(cc);
 
-// 2.
+console.log('----------------------------');
+
+// 2.下面哪个是错误的？ A
+// A. [] == true
+// B. !![] == true
+// C. null == undefined
+// D. new Boolean() == false
+
+console.log([] == true); // false 比较运算符 []空数组 转为 0  true 转为 1
+console.log(!![] == true); // true 转为boolean,空数组转为布尔为true
+console.log(null == undefined); // true  比较运算符 都转为 0
+console.log(new Boolean() == false); // true
+
+
+
+
 
 
