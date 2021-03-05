be1aaa1d4644794a6d8a593419500ba8a00e608a
赵迎超 <zhao_yc1@suixingpay.com>
2021-03-05 10:26:23 +0800

    abc/def/ghi/jkl/mno -> abcdef/ghi/jkl/mno 输入n,删掉对应/实现 1.正则实现 2.利用indexOf实现 3.循环遍历实现

--- src/js/js字符串相关操作/js斜杠位置替换.js
+++ src/js/js字符串相关操作/js斜杠位置替换.js
@@ -25,19 +25,7 @@
 // console.log(process(1));
 // console.log(process(2));
 
-// 利用indexOf
-// let str2 = "abc/def/ghj/klm";
-// function process2(n) {
-//     if (n < 1) return str2;
-//     console.log(str.indexOf("/"));
-//
-//
-// }
-//
-// process2(str2);
-
-
-
+// 遍历实现
 // let str3 = "abc/def/ghj/klm";
 // function process3(n) {
 //     if (n < 1) return str3;
@@ -53,16 +41,49 @@
 // console.log(process3(2));
 
 // 小亮  版本
+// function process(n) {
+//     let regex = new RegExp(`^\\S{${n}}(\\/)?`);
+//     let str = "abc/def/ghj/klm";
+//     return str.replace(regex, function(a,b,c) {
+//         console.log(a,b,c);
+//         return b ? a.substr(0, a.length - 1) : a
+//     });
+// }
+//
+// console.log(process(3));
+
+// 用正则实现，这种比较好 爆赞
+let str = "abc/def/ghj/klm";
 function process(n) {
-    let regex = new RegExp(`^\\S{${n}}(\\/)?`);
-    let str = "abc/def/ghj/klm";
-    return str.replace(regex, function(a,b,c) {
-        console.log(a,b,c);
-        return b ? a.substr(0, a.length - 1) : a
-    });
+    let regex = new RegExp('^(\\w+\\/){' + n + '}', 'g');
+    return str.replace(regex, (aa, bb, cc) => {
+        console.log(aa, bb, cc);
+
+        return aa ? aa.substr(0, aa.length - 1) : "";
+    })
 }
 
-console.log(process(3));
+console.log(process(2));
 
 
+// 利用indexOf 实现 这种实现也不错
+// let str = "abc/def/ghj/klm";
+// function process(n) {
+//     if (isNaN(n)) return;
+//     n = n > 0 ? n - 1 : 0;
+//     const index = find(str, '/', n);
+//     let arr = str.split("");
+//     arr.splice(index, 1);
+//     return arr.join("");
+// }
+//
+// function find(str, cha, num) {
+//     var x = str.indexOf(cha);
+//     for (var i = 0; i < num; i++) {
+//         x = str.indexOf(cha, x + 1);
+//     }
+//     return x;
+// }
+//
+// console.log(process(2));
 
