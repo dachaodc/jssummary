9861177c5156c545fb404634e9036a278dc59080
赵迎超 <zhao_yc1@suixingpay.com>
2021-03-05 10:35:54 +0800

    abc/def/ghi/jkl/mno -> abcdef/ghi/jkl/mno 输入n,删掉对应/实现 1.正则实现 2.利用indexOf实现 3.循环遍历实现

--- src/js/js字符串相关操作/js斜杠位置替换.js
+++ src/js/js字符串相关操作/js斜杠位置替换.js
@@ -1,10 +1,10 @@
-// 调process(n)函数 处理 str = “abc/def/ghj/klm” 输出字符串，去掉对应的位置n的/
-// eg: process(0)  输出 “abc/def/ghj/klm”
-// eg: process(1)  输出 “abcdef/ghj/klm”
-// eg: process(2)  输出 “abc/defghj/klm”
+// 调process(n)函数 处理 str = “abc/def/ghij/klm” 输出字符串，去掉对应的位置n的/
+// eg: process(0)  输出 “abc/def/ghij/klm”
+// eg: process(1)  输出 “abcdef/ghij/klm”
+// eg: process(2)  输出 “abc/defghij/klm”
 
 // 第一种方式 不太好
-// let str = "abc/def/ghj/klm";
+// let str = "abc/def/ghij/klm";
 // function process(n) {
 //     if (n < 1) return str;
 //     let tempArr = [];
@@ -26,7 +26,7 @@
 // console.log(process(2));
 
 // 遍历实现
-// let str3 = "abc/def/ghj/klm";
+// let str3 = "abc/def/ghij/klm";
 // function process3(n) {
 //     if (n < 1) return str3;
 //     let arr = str3.split("");
@@ -43,7 +43,7 @@
 // 小亮  版本
 // function process(n) {
 //     let regex = new RegExp(`^\\S{${n}}(\\/)?`);
-//     let str = "abc/def/ghj/klm";
+//     let str = "abc/def/ghij/klm";
 //     return str.replace(regex, function(a,b,c) {
 //         console.log(a,b,c);
 //         return b ? a.substr(0, a.length - 1) : a
@@ -53,7 +53,7 @@
 // console.log(process(3));
 
 // 用正则实现，这种比较好 爆赞
-let str = "abc/def/ghj/klm";
+let str = "abc/def/ghij/klm";
 function process(n) {
     let regex = new RegExp('^(\\w+\\/){' + n + '}', 'g');
     return str.replace(regex, (aa, bb, cc) => {
@@ -67,7 +67,7 @@ console.log(process(2));
 
 
 // 利用indexOf 实现 这种实现也不错
-// let str = "abc/def/ghj/klm";
+// let str = "abc/def/ghij/klm";
 // function process(n) {
 //     if (isNaN(n)) return;
 //     n = n > 0 ? n - 1 : 0;
