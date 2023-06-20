59686fa454ff71cdf011c2f42261575c0597261b
zhaoyingchao <zhaoyingchao@unipus.cn>
2023-06-20 17:11:35 +0800

    动态规划问题

--- src/js/算法/动态规划/langSub.ts
+++ src/js/算法/动态规划/langSub.ts
@@ -31,23 +31,40 @@ const getItemByNum = (index: number) => {
 
 getItemByNum(10);
 
-// 实现2 
+// 实现2
 function fibonacci(n: number): number[] {
-    // 定义动态规划数组 dp，并初始化 dp[0] 和 dp[1] 的值
-    const dp: number[] = [0, 1];
-    
-    // 递推方式计算 dp[i] 的值
-    for (let i = 2; i <= n; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    
-    // 返回 dp 的值
-    return dp;
+  // 定义动态规划数组 dp，并初始化 dp[0] 和 dp[1] 的值
+  const dp: number[] = [0, 1];
+
+  // 递推方式计算 dp[i] 的值
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
 
-  const ar = fibonacci(5);
-  console.log("total-----2>", ar);
+  // 返回 dp 的值
+  return dp;
+}
 
-// 例子2: 求最长公共子序列
+const ar = fibonacci(5);
+console.log("total-----2>", ar);
 
-//
+// 例子2: 假设有一个长度为 n 的数组 arr，如何找到其中最大的子序列和？其中子序列是指原数组中连续的一段数所组成的序列。
+// 可以使用动态规划的思想，定义一个数组 dp，其中 dp[i] 表示以 arr[i] 结尾的最大子序列和，
+// 那么状态转移方程即为 dp[i] = max(dp[i-1], 0) + arr[i]。其中若 dp[i-1] 大于 0，则表示以 i-1 结尾的最大子序列和对 dp[i] 有贡献，
+// 否则不考虑 dp[i-1]。最终最大子序列和为 dp 数组中的最大值。
+
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]; 求出最大和子序；
+
+const getMax = (arr: number[]) => {
+    const mS = [];
+    for () {
+
+    }
+};
+getMax([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+
+
+//例子3：最长公共子序列问题（Longest Common Subsequence，LCS）是求两个字符串中最长公共子序列的问题。公共子序列不要求连续，只需要在两个字符串中按照顺序出现即可。
+// 例如，对于字符串 "ABCDGH" 和 "AEDFHR"，它们的最长公共子序列是 "ADH"。
+
+
